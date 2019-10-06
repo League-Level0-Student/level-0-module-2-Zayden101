2e3225f9e65f2776ac1dc1798dee26831ae3e745
League Student <league@iMac-55.attlocal.net>
2019-10-06 12:54:21 -0700

    l

--- src/_07_animal_farm/AnimalFarm.java
+++ src/_07_animal_farm/AnimalFarm.java
@@ -19,6 +19,10 @@ public class AnimalFarm {
 		/*
 		 * 1. Ask the user which animal they want, then play the sound of that animal.
 		 */
+		String q=JOptionPane.showInputDialog("WHAT ANIMMAL DO YOU WANT TO LISIEN TOOOOO!!!!!");
+		if (q.equals("cow")) {
+			playMoo();
+		}
 		
 		/* 2. Make it so that the user can keep entering new animals. */
 	}
---
+++ src/lotnum.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class lotnum {
+public static void main(String[] args) {
+	Random n = new Random();
+	int RandomNumber=n.nextInt(6);
+	int RandomNumber1=n.nextInt(5);
+	int RandomNumber2=n.nextInt(4);
+	int RandomNumber3=n.nextInt(3);
+	int RandomNumber4=n.nextInt(2);
+	int RandomNumber5=n.nextInt(1);
+	JOptionPane.showMessageDialog(null, RandomNumber1);
+	JOptionPane.showMessageDialog(null, RandomNumber2);
+	JOptionPane.showMessageDialog(null, RandomNumber3);
+	JOptionPane.showMessageDialog(null, RandomNumber4);
+	JOptionPane.showMessageDialog(null, RandomNumber5);
+	JOptionPane.showMessageDialog(null, RandomNumber);
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
